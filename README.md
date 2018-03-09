7756d2734af8619e19b4ad6df8584677406fe2d8
rlatmfrl24 <rlatmfrl24@naver.com>
2018-03-09 14:41:01 +0900

    Package 및 Code 정리, Jar 파일 첨부

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # HTML Content Extractor
 
-1. 
+0. 개요
+
+HTML 파일을 분석하여 본문 내용을 추출하는 모듈
+
+1. 사용법
+
+java -jar Extractor.jar "[Language Code]" "[Input_Path]" "[Output_Path]"
+
+- Language Code : 언어코드(e.g. ko_kr, en_us)
+- Input_Path : HTML 파일의 폴더 경로
+- Output_Path : Extraction 결과를 저장할 폴더 경로
+
--- src/DBCE/Filter/RepeatLineMap.java
+++ src/DBCE/Filter/RepeatLineMap.java
@@ -1,5 +1,13 @@
 package DBCE.Filter;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map.Entry;
@@ -10,7 +18,75 @@ public class RepeatLineMap {
 	private HashMap<String, Integer> RepeatMap = new HashMap<>();
 	private LinkedList<Diff> dlist;
 	
-	public RepeatLineMap(){
+	public void refineByMap(String input_path, String output_path) {
+		String content = "";
+		String before = "";
+		File output_file = new File(output_path);
+		if(!output_file.exists()){
+			output_file.mkdirs();
+		}
+		
+		try{
+			File f = new File(input_path);
+			for (File rf : f.listFiles()) {
+				if (rf.isFile()) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(rf), StandardCharsets.UTF_8));
+					StringBuilder str = new StringBuilder();
+					char[] c = new char[(int)rf.length()];
+					br.read(c);
+					str.append(c);
+					content = str.toString();
+					br.close();
+					System.out.println("=> Find Repeat Phreas By "+rf.getName());
+					UpdateMap(before, content);
+					before = content;
+					content = "";
+					br.close();
+				}
+			}
+			Resize(2);
+			System.out.println(getRepeatMap());
+			
+			for (File rf : f.listFiles()) {
+				if (rf.isFile()) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(rf), StandardCharsets.UTF_8));
+					BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(output_path + rf.getName())), StandardCharsets.UTF_8));
+					StringBuilder str = new StringBuilder();
+					char[] c = new char[(int)rf.length()];
+					br.read(c);
+					str.append(c);
+					content = str.toString();
+					br.close();
+					System.out.println(rf.getName());
+					bw.write(refine(content, true));
+					bw.flush();
+					bw.close();
+					content = "";
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void subDirList(String source){
+		File dir = new File(source); 
+		File[] fileList = dir.listFiles();
+		
+		try{
+			for(int i = 0 ; i < fileList.length ; i++){
+				File file = fileList[i]; 
+				if(file.isFile()){
+					file.delete();
+				}else if(file.isDirectory()){
+					subDirList(file.getCanonicalPath().toString()); 
+					file.delete();
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
 	}
 	
 	public void Resize(int weight){
@@ -54,7 +130,7 @@ public class RepeatLineMap {
 		return this.RepeatMap;
 	}
 	
-	public String refineByMap(String origin, boolean reverseOption) {
+	public String refine(String origin, boolean reverseOption) {
 		String res = "";
 		if (reverseOption) {
 			for (String line : origin.split("\n")) {
